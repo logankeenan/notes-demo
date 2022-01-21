90bc977934e10b97b0eb83ee5fe7998f09eba1e3
Logan Keenan <d.logankeenan@gmail.com>
2022-01-21 05:52:38 -0600

    make root path load all notes

--- src/router.rs
+++ src/router.rs
@@ -3,6 +3,7 @@ use crate::controllers::note_controller;
 
 pub fn setup(app: &mut Server<()>) {
     app.at("/notes").get(note_controller::index);
+    app.at("/").get(note_controller::index);
     app.at("/notes/new").get(note_controller::new);
     app.at("/notes").post(note_controller::create);
     app.at("/notes/edit/:id").get(note_controller::edit);
